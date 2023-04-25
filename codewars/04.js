27f0186776c4cb1fec06874dfcd9037d98025c0c
Maksym Vedmedenko <maksym.vedmedenko@gmail.com>
2023-04-24 23:33:10 -0400

    codewars 04

--- codewars/04.js
+++ codewars/04.js
@@ -1,6 +1,3 @@
-function validateHello(greetings) {
-    const langues = ["hello", "ciao", "salut", "hallo", "hola", "ahoj", "czesc"]
-    return langues.map((i) => i === greetings ? true : false)
-  }
-
-  validateHello('ahoj');
+function removeChar(str){
+    return str.slice(1, -1)
+  };
